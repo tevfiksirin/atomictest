a9e0c0f0bd16d3dc9631801fba0378ae4b2f0944
tevfiksirin <tevfiksirin@gmail.com>
2021-02-15 16:54:52 +0300

    Update WorkHeader.vue

--- components/global/WorkHeader.vue
+++ components/global/WorkHeader.vue
@@ -11,6 +11,26 @@
             height="64"
           />
         </div>
+        <div class="flex items-center md:hidden">
+          <button
+            @click="isOpen = !isOpen"
+            type="button"
+            class="text-indigo-900 focus:text-indigo-900 focus:outline-none hover:text-pink-600"
+          >
+            <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
+              <path
+                v-if="isOpen"
+                fill-rule="evenodd"
+                d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+              />
+              <path
+                v-if="!isOpen"
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              />
+            </svg>
+          </button>
+        </div>
         <div class="flex justify-end">
           <div
             :class="isOpen ? 'block' : 'hidden'"
@@ -27,26 +47,6 @@
                 <nuxt-link class="" to="/work">Work</nuxt-link>
               </li>
             </ul>
-          </div>
-          <div class="flex items-center md:hidden">
-            <button
-              @click="isOpen = !isOpen"
-              type="button"
-              class="text-indigo-900 focus:text-indigo-900 focus:outline-none hover:text-pink-600"
-            >
-              <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
-                <path
-                  v-if="isOpen"
-                  fill-rule="evenodd"
-                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                />
-                <path
-                  v-if="!isOpen"
-                  fill-rule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                />
-              </svg>
-            </button>
           </div>
         </div>
       </header>
