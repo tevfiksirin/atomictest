ea5955f1669b095d635d5d22d1b6cf622f4455ed
tevfiksirin <tevfiksirin@gmail.com>
2021-02-15 13:04:33 +0300

    Update WorkHeader.vue

--- components/global/WorkHeader.vue
+++ components/global/WorkHeader.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="w-full h-16 bg-indigo-200">
-    <div class="container mx-auto bg-red-400">
-      <header class="flex justify-around">
+  <div class="w-full h-auto">
+    <div class="container h-auto mx-auto">
+      <header class="flex justify-between">
         <img
-          class="p-5"
+          class=""
           src="https://bradlc.gallerycdn.vsassets.io/extensions/bradlc/vscode-tailwindcss/0.4.3/1598388191194/Microsoft.VisualStudio.Services.Icons.Default"
           alt="logo"
           width="64"
           height="64"
         />
-        <nav class="p-5 bg-yellow-300">
-          <ul>
-            <li>
-              <nuxt-link to="/">Anasayfa</nuxt-link>
-              <nuxt-link to="/dost">Dost</nuxt-link>
-              <nuxt-link to="/work">Work</nuxt-link>
-            </li>
-          </ul>
-        </nav>
+        <ul class="flex justify-end w-5/12 gap-x-1">
+          <li class="p-2 place-self-center">
+            <nuxt-link class="" to="/">Anasayfa</nuxt-link>
+          </li>
+          <li class="p-2 place-self-center">
+            <nuxt-link class="" to="/dost">Dost</nuxt-link>
+          </li>
+          <li class="p-2 place-self-center">
+            <nuxt-link class="" to="/work">Work</nuxt-link>
+          </li>
+        </ul>
       </header>
     </div>
   </div>
